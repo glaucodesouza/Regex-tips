66dadbd828ce046c5fb76a9a812414d7de001c04
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-08-15 20:23:38 -0300

    meta caracteres

--- metaCaracteres.js
+++ metaCaracteres.js
@@ -1,5 +1,23 @@
 // . ? * + - ^ $  | [ ] { } ( ) \ : \
 
-const texto = '1,2,3,4,5,6,a.b c!d?e';
-const regexPonto
+//split no caracter ponto,
+//retornando 2 arrays
+const texto = "1,2,3,4,5,6,a.b c!d?e";
+const regexPonto = /\./g;
+console.log(texto.split(regexPonto));
 
+//split por meta caracteres
+// split por:
+//,
+//.
+//?
+//!
+//espaco
+const regexSimbolos = /,|\.|\?|!| /g;
+console.log(texto.split(regexSimbolos));
+// vai gerar este array o split:
+//[
+//  '1', '2', '3', '4',
+//  '5', '6', 'a', 'b',
+//  'c', 'd', 'e'
+//]
