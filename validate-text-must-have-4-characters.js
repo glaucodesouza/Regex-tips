a7320c976b180ff60ddd3722f84b39672b4e3e6a
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-08-30 15:36:16 -0300

    validate text must have 4 characteres

---
+++ validate-text-must-have-4-characters.js
@@ -0,0 +1,12 @@
+/*
+When you want to get just a middle value 10999
+from the text bellow.
+
+Use Extension Code Runner
+to test your code in VS Code terminal
+*/
+let text = `DD99`;
+let isValid = text.match(/^[a-zA-Z0-9]{4}$/);
+/*results
+isValid=true
+*/
