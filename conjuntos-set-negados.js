e60718b18ad01e2440a0e597eab7b4509b4e2d29
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-08-30 16:35:24 -0300

    negating sets (conjuntos)

---
+++ conjuntos-set-negados.js
@@ -0,0 +1,57 @@
+/*
+When you want using:
+  Negate a set (conjunto)
+
+Use Extension Code Runner
+to test your code in VS Code terminal
+*/
+
+let text = `1,2,3,4,5,6,a.b c!d?e[f`;
+
+console.log(text.match(/\D/g)); // Negate numbers [^0-9]
+/*results
+[
+  ',', ',', ',', ',', ',',
+  ',', 'a', '.', 'b', ' ',
+  'c', '!', 'd', '?', 'e',
+  '[', 'f'
+]
+*/
+
+console.log(text.match(/[^0-9]/g)); // Negate numbers [^0-9]
+/*results, same result as above.
+[
+  ',', ',', ',', ',', ',',
+  ',', 'a', '.', 'b', ' ',
+  'c', '!', 'd', '?', 'e',
+  '[', 'f'
+]
+*/
+
+console.log(text.match(/[^\d!\?\[\s,\.]/g)); // Negates numbers, !, ?, [, space and .
+/*
+explanation: it is simply the same as \d
+^ = Negates
+\d = numbers
+! = exclamation
+\? = interrogation
+\[ = colchete
+\s = space, enter, linefeed
+, = comma
+\. = point
+ numbers, !, ?, [, space and .
+
+results:
+[ 'a', 'b', 'c', 'd', 'e', 'f' ]
+*/
+
+console.log(text.match(/[^\d]/g)); // Negate digits
+/*
+results:
+[
+  ',', ',', ',', ',', ',',
+  ',', 'a', '.', 'b', ' ',
+  'c', '!', 'd', '?', 'e',
+  '[', 'f'
+]
+*/
