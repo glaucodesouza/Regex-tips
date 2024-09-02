453e1a5218ee438b934a331e7a66d08dfa84b3fc
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-09-02 11:45:32 -0300

    quantificador {n,m}

--- quantifiers-intervals.js
+++ quantifiers-intervals.js
@@ -8,9 +8,9 @@ to test your code in VS Code terminal
 
 let text1 = `O João recebeu 120 milhões apostando 6 9 21 23 45 46.`;
 
-console.log(text1.match(/\d{1,2}/g));
-console.log(text1.match(/[0-9]{2}/g));
-console.log(text1.match(/\d{1,}/g));
+console.log(text1.match(/\d{1,2}/g)); //apenas números c/ 1 ou dois dígitos
+console.log(text1.match(/[0-9]{2}/g)); //apenas números com 2 dígitos
+console.log(text1.match(/\d{1,}/g)); //apenas números c/ 1 ou mais dígitos
 console.log(text1.match(/\w{7}/g)); //aqui não traz milhões, porque a busca parou no õ, não estava na sequência da tabela ascii para o qualificador \w
 console.log(text1.match(/[\wõ]{7,}/g)); // então, aqui, adicionamos o õ, para continuar buscando a palavra completa milhões, quando passar pelo õ
 /*results
