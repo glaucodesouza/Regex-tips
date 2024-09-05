3de2da5227ba74735883b88694d21ecd501c7cb8
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-09-05 16:15:48 -0300

    groups

--- groups-2-html-tags.js
+++ groups-2-html-tags.js
@@ -10,9 +10,39 @@ let text1 = `<b>Destaque</b><strong>Forte</strong><div>Content</div>`;
 console.log(text1.match(/<(\w+)>.*<\/\1>/g)); // g to get all references
 
 /*
-results:
+explaining regex:
 <(\w+)>   => to get <tags>
 .*        => to get words/texts between tags
 <\/\1>    => to get closing tags </tag> (important: \1 references the group 1, to get group 1 again)
 
+results:
+[ '<b>Destaque</b>', '<strong>Forte</strong>', '<div>Content</div>' ]
 */
+
+let text2 = `Lentamente é mente muito lenta.`;
+console.log(text2.match(/(lenta)(mente).*\2.*\1\./gi));
+
+// ?: não guarda o valor do grupo usado nele. 
+// Então \1 guardou apenas a segundo grupo (mente) como primeiro.
+console.log(text2.match(/(?:lenta)(mente).*\1/gi));
+
+console.log(text2.match(/(lenta)(mente)?/gi)); // (mente)? é um grupo com captura opcional
+
+
+/*
+explaining regex:
+  (lenta)   ==> first capturing
+  (mente)   ==> second capturing
+  .*        ==> capturing text ' é '
+  \2        ==> represents the second capturing 'mente'
+  .*        ==> the rest of text ' muito '
+  \1        ==> represents the first capturing 'lenta'
+  \.        ==> captures the final point '.'
+
+results:
+  [ '<b>Destaque</b>', '<strong>Forte</strong>', '<div>Content</div>' ]
+  [ 'Lentamente é mente muito lenta.' ]
+  [ 'Lentamente é mente' ]
+  [ 'Lentamente', 'lenta' ]
+*/
+
